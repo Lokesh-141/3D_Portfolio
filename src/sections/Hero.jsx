b0cf78241e3d306ded4a086a4336d5058d70a9da
Lokesh-141 <166227979+Lokesh-141@users.noreply.github.com>
2026-01-07 23:24:29 +0530

    Update Hero.jsx

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -22,7 +22,7 @@ const Hero = () => {
                             <h1>into Real Projects</h1>
                             <h1>that Deliver Results</h1>
                         </div>
-                        <p className='text-white-50 md:text-xl relative z-10 pointer-events-none'> Hi, I’m Kairav, a developer based in Croatia with a passion for code.</p>
+                        <p className='text-white-50 md:text-xl relative z-10 pointer-events-none'> Hi, I’m Kairav, a developer based in India with a passion for code.</p>
                         <Button text='See My Work' className='md:w-80 md:h-16 w-60 h-12' id='counter' />
                     </div>
                 </header>
